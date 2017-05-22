b28b052e57f6d1e6cd2c507796adc285073b8d7a
Ryann Consalo <ryannconsalo@students-MacBook-Pro-26.local>
2017-05-22 14:08:32 -0400

    Initial Commit

---
+++ iOSWeek2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSWeek2
+//
+//  Created by Ryann Consalo on 5/22/17.
+//  Copyright © 2017 Ryann Consalo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
